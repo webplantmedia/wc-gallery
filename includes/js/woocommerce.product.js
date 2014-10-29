564a51a08a4b940fd114ca106e4946753e5983c7
Chris Baldelomar <chris@webplantmedia.com>
2014-10-29 17:20:01 -0500

    supporting lightbox for WooCommerce galleries

--- includes/js/woocommerce.product.js
+++ includes/js/woocommerce.product.js
@@ -9,6 +9,12 @@
 
 	$(document).ready(function(){
 		if( jQuery().magnificPopup) {
+			$("a[data-rel^='prettyPhoto']").magnificPopup({
+				gallery: {
+					enabled: true
+				},
+				type:'image',
+			});
 			$("a[rel^='prettyPhoto']").magnificPopup({
 				gallery: {
 					enabled: true
