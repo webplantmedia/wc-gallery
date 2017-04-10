713f3f62756f17af48b7f889c2267ac7dda48228
Chris Baldelomar <chris@webplantmedia.com>
2017-04-10 13:32:18 -0500

    removed document ready

--- includes/js/woocommerce.product.js
+++ includes/js/woocommerce.product.js
@@ -7,20 +7,18 @@
 ( function( $ ) {
 	"use strict";
 
-	$(document).ready(function(){
-		if( jQuery().magnificPopup) {
-			$("a[data-rel^='prettyPhoto']").magnificPopup({
-				gallery: {
-					enabled: true
-				},
-				type:'image',
-			});
-			$("a[rel^='prettyPhoto']").magnificPopup({
-				gallery: {
-					enabled: true
-				},
-				type:'image',
-			});
-		}
-	});
+	if( jQuery().magnificPopup) {
+		$("a[data-rel^='prettyPhoto']").magnificPopup({
+			gallery: {
+				enabled: true
+			},
+			type:'image',
+		});
+		$("a[rel^='prettyPhoto']").magnificPopup({
+			gallery: {
+				enabled: true
+			},
+			type:'image',
+		});
+	}
 } )( jQuery );
